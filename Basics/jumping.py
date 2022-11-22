84d9a5ade932a6e79368ffb06cdccef967462375
Sourav Kumar <souravk326@gmail.com>
2022-11-22 14:02:57 +0000

    jumping number

--- Basics/jumping.py
+++ Basics/jumping.py
@@ -1,1 +1,14 @@
+num = int(input("Enter the Number:- "))
+n = num
+flag = True
+while num!= 0:
+    num1 = num%10
+    num = num/10
+    num2 = num%10
+    if abs(num1 - num2) != 1:
+        flag = False
 
+if flag == True:
+    print(n,"it is jumping number")
+else:
+    print(n,"it is not a jumping number")
